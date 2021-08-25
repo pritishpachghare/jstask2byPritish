909227c7dc85e4a74f4e22824b5209dad221b9fe
Pritish Pachghare <pritishpachghare@gmail.com>
2021-08-24 19:15:16 -1200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const recipesArray = [
+    {
+        meal: "pancakes",
+        ingredients: ["eggs", "flour", "milk", "butter"],
+        time: "0.5 hour",
+    },
+    {
+        meal: "tomato soup",
+        ingredients: ["eggs", "flour", "milk", "butter"],
+        time: "1.5 hour",
+    },
+    {
+        meal: "eggs with salad",
+        ingredients: ["eggs", "flour", "milk", "butter"],
+        time: "1 hour",
+    },
+    {
+        meal: "cake",
+        ingredients: ["eggs", "flour", "milk", "butter"],
+        time: "2 hour",
+    },
+    {
+        meal: "soup",
+        ingredients: ["tomato", "flour", "milk", "butter"],
+        time: "1 hour",
+    },
+];
+
+let recepe = prompt("Recepe name");
+
+for (let recp of recipesArray) {
+    if (recp.meal.toLowerCase() === recepe.toLocaleLowerCase()) {
+        console.log(' Recepe: ' + recp.meal + " takes " + recp.time + " and ingredients required are :"
+            + recp.ingredients);
+        break;
+    }
+    if (recp.meal === recipesArray[recipesArray.length - 1].meal) {
+        console.log(' Recepe: ' + "Sorry recepe is not in the list.")
+    }
+}
